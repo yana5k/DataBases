8c159489b935d19bac8e746ad36174c4c71be297
Yana <43292779+yana5k@users.noreply.github.com>
2020-04-19 15:59:46 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,51 @@
+
+---INSERTING DATA INTO Artists
+
+INSERT INTO Artists VALUES('The Beatles');
+INSERT INTO Artists VALUES('AC/DC');
+INSERT INTO Artists VALUES('B.B. King');
+INSERT INTO Artists VALUES('Bob Dylan');
+INSERT INTO Artists VALUES('Black Sabbath');
+INSERT INTO Artists VALUES('Pixies');
+INSERT INTO Artists VALUES('The Doors');
+INSERT INTO Artists VALUES('Bob Marley & The Wailers');
+INSERT INTO Artists VALUES('Arctic Monkeys');
+INSERT INTO Artists VALUES('Pink Floyd');
+
+
+---INSERTING DATA INTO Genres
+INSERT INTO Genres VALUES('Rock');
+INSERT INTO Genres VALUES('Funk / Soul');
+INSERT INTO Genres VALUES('Pop');
+INSERT INTO Genres VALUES('Blues');
+INSERT INTO Genres VALUES('Reggae');
+INSERT INTO Genres VALUES('Jazz');
+INSERT INTO Genres VALUES('Hip Hop');
+INSERT INTO Genres VALUES('Folk');
+INSERT INTO Genres VALUES('Latin');
+INSERT INTO Genres VALUES('Electronic');
+
+---INSERTING DATA INTO AlbumInfo
+INSERT INTO AlbumInfo VALUES(1, 'Black Sabbath', 'Paranoid', 1970);
+INSERT INTO AlbumInfo VALUES(2, 'Pink Floyd', 'The Dark Side of the Moon', 1973);
+INSERT INTO AlbumInfo VALUES(3, 'The Doors', 'The Doors', 1967);
+INSERT INTO AlbumInfo VALUES(4, 'AC/DC', 'Highway to Hell', 1979);
+INSERT INTO AlbumInfo VALUES(5,'Bob Marley & The Wailers', 'Legend: The Best of Bob Marley and The Wailers', 1984);
+INSERT INTO AlbumInfo VALUES(6, 'B.B. King', 'Live in Cook County Jail', 1971);
+INSERT INTO AlbumInfo VALUES(7, 'B.B. King', 'Live at the Regal', 1965);
+INSERT INTO AlbumInfo VALUES(8, 'Black Sabbath', 'Black Sabbath', 1970);
+INSERT INTO AlbumInfo VALUES(9, 'Pixies', 'Doolittle', 1989);
+INSERT INTO AlbumInfo VALUES(10, 'Bob Dylan', 'Blonde on Blonde', 1966);
+
+---INSERTING DATA INTO AlbumGenre
+INSERT INTO AlbumGenre VALUES(1, 'Rock');
+INSERT INTO AlbumGenre VALUES(2, 'Rock');
+INSERT INTO AlbumGenre VALUES(3, 'Rock');
+INSERT INTO AlbumGenre VALUES(4, 'Rock');
+INSERT INTO AlbumGenre VALUES(5, 'Reggae');
+INSERT INTO AlbumGenre VALUES(6, 'Blues');
+INSERT INTO AlbumGenre VALUES(7, 'Blues');
+INSERT INTO AlbumGenre VALUES(8, 'Rock');
+INSERT INTO AlbumGenre VALUES(9, 'Rock');
+INSERT INTO AlbumGenre VALUES(10, 'Rock');
+INSERT INTO AlbumGenre VALUES(10, 'Blues');
